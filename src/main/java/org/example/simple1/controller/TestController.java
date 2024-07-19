6f9cb15fea72284f812121d1a0de1b4aad73b8b8
Anseongmin5739 <smahn4069@gmail.com>
2024-07-19 15:01:32 +0900

    sm ch lets go

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @CrossOrigin(origins = "*")
 public class TestController {
@@ -14,5 +16,10 @@ public class TestController {
         memberDto.setName("test");
         memberDto.setAge(20);
         return memberDto;
+    }
+
+    @GetMapping("/test/sm")
+    public Map<String,String> testSm() {
+        return Map.of("성민","창호");
     }
 }
